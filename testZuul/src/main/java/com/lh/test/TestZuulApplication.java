d1f11cffe4231289e946b080cd7c20e464d3aa91
root <892969067@qq.com>
2019-07-17 15:10:59 +0800

    1

--- testZuul/src/main/java/com/lh/test/TestZuulApplication.java
+++ testZuul/src/main/java/com/lh/test/TestZuulApplication.java
@@ -11,7 +11,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class TestZuulApplication {
 
 	public static void main(String[] args) {
-		System.out.println("1212121212");
 		SpringApplication.run(TestZuulApplication.class, args);
 	}
 
